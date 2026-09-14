78cd9b1b04a9cf58213612551ea11aa65c1a0a38
Kevin K. Lux-Sullivan <kevinsulli@gmail.com>
2026-09-13 23:56:13 -0400

    lab1

--- src/module_1/lab1_recommendations.py
+++ src/module_1/lab1_recommendations.py
@@ -1,1 +1,27 @@
 # Refer to this module's readme
+
+
+def main():
+    difficulty = input("Difficult or Casual? ")
+    if not (difficulty == "Difficult" or difficulty == "Casual"):
+        print("Enter a valid difficulty")
+        return
+    
+    players = input("Multiplayer or Single-player? ")
+    if not (players == "Multiplayer" or players == "Single-player"):
+        print("Enter a valid number of players")
+        return
+
+    if difficulty == "Difficult" and players == "Multiplayer":
+             recommend("Poker")
+    elif difficulty == "Difficult" and players == "Single-player":
+            recommend("Klondike")
+    elif difficulty == "Casual" and players == "Multiplayer":
+            recommend("Hearts")
+    elif difficulty == "Casual" and players == "Single-player":
+            recommend("Clock")
+
+def recommend(game):
+        print(f"You might like {game}")
+main()
+
