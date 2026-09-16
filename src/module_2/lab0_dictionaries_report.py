331691580bc55162cee75ff96c80e964ef94b231
Kevin K. Lux-Sullivan <kevinsulli@gmail.com>
2026-09-16 17:05:23 -0400

    that's a commit message

--- src/module_2/lab0_dictionaries_report.py
+++ src/module_2/lab0_dictionaries_report.py
@@ -1,1 +1,17 @@
 # Refer to this module's readme
+def main():
+    spacecraft = {"name": "James Webb Space Telescope"}
+    spacecraft.update({"distance": 0.01, "orbit": "Sun"})
+    print(create_report(spacecraft))
+
+def create_report(spacecraft):
+    return f"""
+========== REPORT ==========
+
+Name: {spacecraft.get("name", "Unknown")}
+Distance: {spacecraft.get("distance", "Unknown")} AU
+Orbit: {spacecraft.get("orbit", "Unknown")}
+============================
+"""
+
+main()
