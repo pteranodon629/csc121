85ab42adc5e99f3db2e6ae6a4d3e5efd1e7574f4
Kevin K. Lux-Sullivan <kevinsulli@gmail.com>
2026-09-17 09:55:57 -0400

    lists

--- src/module_2/lab3_lists_results.py
+++ src/module_2/lab3_lists_results.py
@@ -1,1 +1,7 @@
-# Refer to this module's readme
+results = ["Mario", "Luigi", "Princess", "Yoshi", "Koopa Troopa", "Toad", "Bowser", "Donkey Kong Jr."]
+
+results.remove("Bowser")
+results.insert(0,"Bowser")
+results.reverse()
+
+print(results)
