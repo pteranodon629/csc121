692c3763b5bdb4d644767c57cb4ea5768f90147b
Kevin K. Lux-Sullivan <kevinsulli@gmail.com>
2026-09-18 18:38:08 -0400

    removed colon

--- src/module_2/lab9_water.py
+++ src/module_2/lab9_water.py
@@ -3,12 +3,12 @@ from soil import sample
 def main():
    moisture = sample()
    days = 0
-   print(f"Day {days}: Moisture is: {moisture}%")
+   print(f"Day {days}: Moisture is {moisture}%")
 
    while moisture > 20:
         moisture = sample()
         days += 1
-        print(f"Day {days}: Moisture is: {moisture}%")
+        print(f"Day {days}: Moisture is {moisture}%")
 
    print("Time to water!")
 
