6d471384659b2f6af633afe4db6ec25521ee4e1c
Kevin K. Lux-Sullivan <kevinsulli@gmail.com>
2026-09-18 17:39:32 -0400

    add lab6

--- src/module_2/lab6_shows.py
+++ src/module_2/lab6_shows.py
@@ -1,1 +1,18 @@
-# Refer to this module's readme
+SHOWS = [
+    " Avatar: the last airbender",
+    "Ben 10",
+    "Arthur",
+    " SpongeBob SquarePants",
+    "Phineas and ferb",
+    "Kim possible",
+    "Jimmy Neutron ",
+    "the Proud Family"
+]
+
+def main():
+    cleaned_shows = []
+    for show in SHOWS:
+        cleaned_shows.append(show.strip().title())
+    print(', '.join(cleaned_shows))
+
+main()
