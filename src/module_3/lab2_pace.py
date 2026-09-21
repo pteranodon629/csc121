1186d341423bbffda3ecb37414d9a4bb756878ea
Kevin K. Lux-Sullivan <kevinsulli@gmail.com>
2026-09-20 22:19:23 -0400

    pace

--- src/module_3/lab2_pace.py
+++ src/module_3/lab2_pace.py
@@ -1,1 +1,12 @@
-# Refer to this module's readme
+def main():
+    pace = get_pace(miles=26.2, minutes=0)
+    print(f"You need to run each mile is {round(pace, 2)} minutes.")
+
+def get_pace(miles, minutes):
+    if not minutes > 0:
+        raise ValueError("Invalid value for minutes.")
+    
+    return minutes / miles
+
+
+main()
