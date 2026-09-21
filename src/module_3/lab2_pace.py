707e21048d138ca319d69546dac7677302238483
Kevin K. Lux-Sullivan <kevinsulli@gmail.com>
2026-09-20 22:21:25 -0400

    updated message

--- src/module_3/lab2_pace.py
+++ src/module_3/lab2_pace.py
@@ -4,7 +4,7 @@ def main():
 
 def get_pace(miles, minutes):
     if not minutes > 0:
-        raise ValueError("Invalid value for minutes.")
+        raise ValueError("Minutes must be greater than 0")
     
     return minutes / miles
 
